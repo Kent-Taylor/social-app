8160550e1c06ac52657094bbb2800482c46f70d3
Raisa Bustos <raisa.bustosv@gmail.com>
2019-08-06 11:08:11 -0600

    Rendering data from API - Read method from CRUD

--- src/index.js
+++ src/index.js
@@ -1,46 +1,41 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-import mockData from "./mockData";
 import UserProfile from "./userProfile"
 
 import "./styles.css";
+const App = () => {
+  const [users, setUsers] = React.useState([]);
 
-class App extends React.Component {
-  constructor(){
+  React.useEffect(()=>{
+    fetch("https://social-app-backend-bot.herokuapp.com/users")
+   .then(response => response.json())
+   .then(data => setUsers(data))
+   .catch(error => console.log(error));
+  }, []);
 
-    super();
+  const renderUsers = () => {
+    return users.map(user => {
+      return <UserProfile user={user} />;
+    });
+  };
 
-    this.state = {
-      users: mockData
-    };
-  }
-  renderUsers = () => {
-    return this.state.users.map(user => {
-      return <UserProfile user={user} />
-    })
-  } 
-  
- render() {
   return (
-    <div className="App">
+  <div className="App">
      <div className="navbar-wrapper">
-        <div className="logo">desc. logo</div>
-        <div className="btn">
-          <button>newbutton</button>
-          <button>newbutton</button>
-          <button>newbutton</button>
-          <button>newbutton</button>
-        </div>
+      <div className="logo">desc. logo</div>
+      <div className="btn">
+        <button>newbutton</button>
+        <button>newbutton</button>
+        <button>newbutton</button>
+        <button>newbutton</button>
+      </div>
       </div> 
       <div className="user-wrapper"> 
-      {this.renderUsers()}
-      </div>
-    </div> 
-    )
-}
-
-}
+      {renderUsers()}
+      </div>  
+  </div>)
+  ;
+};
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
--- src/userProfile.js
+++ src/userProfile.js
@@ -1,25 +1,20 @@
 import React from "react";
 
-class UserProfile extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {};
-  }
-
-  render() {
-    const user = this.props.user;
-
-    return (
-      <div className="user-profile">
-        <img src={user.image} alt="user pic" />
-        <h2>
-          {user.name}
-        </h2>
-        <p className="user-bio">{user.short_bio}</p>
-      </div>
-    );
-  }
-}
+const UserProfile = props => {
+  
+  return (
+    <div className="user-profile">
+      <img src={props.user.image} alt="user pic"/>
+      <h2>{props.user.name}</h2>
+      <p className="user-bio">{props.user.short_bio}</p>
+      <div className="edit-section">
+          <div className="edit-button">
+            <button>Edit</button>
+          </div>
+          <div className="delete-button">X</div>
+        </div>
+    </div>
+  );
+};
 
 export default UserProfile;
