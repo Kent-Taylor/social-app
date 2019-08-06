dff07a449bc042a5a1e2602ab7738b2fa31470a6
Emerson Silva <52221684+pexaos@users.noreply.github.com>
2019-08-06 10:26:21 -0600

    added navbar

--- src/index.js
+++ src/index.js
@@ -1,13 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import Navbar from "./pages/navbar";
+import Home from "./pages/home";
+
+import About from "./pages/about";
+import Message from "./pages/message";
+import Picts from "./pages/picts";
 
 import "./styles.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello Team 2, here is our starter Social App!</h1>
-      <h2>Whoooot!</h2>
+      <BrowserRouter>
+        <div className="navbar-wrapper">
+          <div className="logo">desc. logo</div>
+          <Navbar />
+
+          <div className="userName">
+            <div className="login">Login</div>
+            <button className="btn">USER NAME</button>
+            <button className="btn">PASSWORD</button>
+          </div>
+        </div>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/picts" component={Picts} />
+          <Route path="/about" component={About} />
+          <Route path="/message" component={Message} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
